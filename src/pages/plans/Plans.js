b6bce2e4ef57e6a623d10fcb5e50389c671aa022
coderithik@gmail.com <coderithik@gmail.com>
2024-05-04 18:34:49 +0530

    Alignment fixes

--- src/pages/plans/Plans.js
+++ src/pages/plans/Plans.js
@@ -2,12 +2,16 @@ import HomeToolbarLayout from "../../layouts/home/HomeToolbarLayout";
 import IntroWelcomeSection from "./section1/IntroWelcomeSection";
 import PlanDetails from "./section2/PlanDetails";
 import Footer from "../../component/footer/Footer";
+import useIsMobile from "../../hooks/useIsMobile";
+import { SectionContainerLeft } from "../home/sections/SectionContainer";
 
 const Plans = () => {
+  const isMobile = useIsMobile();
   return (
     <HomeToolbarLayout highlighted={1}>
       <IntroWelcomeSection />
       <PlanDetails />
+      {isMobile && <SectionContainerLeft styles={{ minHeight: "20vh" }} />}
       <Footer />
     </HomeToolbarLayout>
   );
